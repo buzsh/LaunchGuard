1deb27d6fad478374c4face58a9cf28f29c990d0
Justin Bush <jubush@icloud.com>
2024-03-06 11:58:38 -0700

    feat: SwiftUI project

---
+++ LaunchGuard/LaunchGuard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaunchGuard
+//
+//  Created by Justin Bush on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ LaunchGuard/LaunchGuard/LaunchGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchGuardApp.swift
+//  LaunchGuard
+//
+//  Created by Justin Bush on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LaunchGuardApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
