332a4a92df6f54471c16c60383520c8aecd85994
Justin Bush <jubush@icloud.com>
2024-03-07 09:57:36 -0700

    refactor: isRemovable for dynamic directories

--- LaunchGuard/DaemonGuard/DirectoryObserver.swift
+++ LaunchGuard/DaemonGuard/DirectoryObserver.swift
@@ -9,14 +9,20 @@ import Foundation
 import SwiftUI
 import Combine
 
+extension Constants {
+  static let directoryRefereshIntervalInSeconds: UInt64 = 1
+}
+
 class DirectoryObserver: ObservableObject {
   @Published var files: [String] = []
   
-  private var directoryURL: URL
+  var directoryURL: URL
   private var observationTask: Task<Void, Never>?
+  var isRemovable: Bool
   
-  init(directoryURL: URL) {
+  init(directoryURL: URL, isRemovable: Bool = true) {
     self.directoryURL = directoryURL
+    self.isRemovable = isRemovable
   }
   
   func startObserving() {
@@ -24,7 +30,7 @@ class DirectoryObserver: ObservableObject {
     observationTask = Task { [weak self] in
       while !Task.isCancelled {
         await self?.updateFileList()
-        try? await Task.sleep(nanoseconds: 1_000_000_000) // Sleep for 1 second
+        try? await Task.sleep(nanoseconds: Constants.directoryRefereshIntervalInSeconds * 1_000_000_000)
       }
     }
   }
